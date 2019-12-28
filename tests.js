af739bd3e7211c90193fd49eaf880b306153e175
Richard Vernon Dyson <dyson77z@gmail.com>
2019-12-28 06:44:15 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -1,8 +1,10 @@
+var name = 'Susan'
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
 }
+var height = 70
 if (height === "70") {
     console.log("The height is correct")
 } else {
